44657da7dbb1b2eb1d061ac95645a84aabbbf1d3
Landon Ellis <landon.ellis2@gmail.com>
2025-10-06 20:38:05 -0400

    Ability buttons

--- Golf/Assets/Scripts/InputBridge.cs
+++ Golf/Assets/Scripts/InputBridge.cs
@@ -13,4 +13,19 @@ public class InputBridge : MonoBehaviour
         PlayerInput.sendInput(PlayerInput.Axis.Cancel);
     }
 
+    public void abilityButton()
+    {
+        PlayerInput.sendInput(PlayerInput.Axis.Fire3);
+    }
+
+    public void swapUpButton()
+    {
+        PlayerInput.sendInput(PlayerInput.Axis.SwapUp);
+    }
+
+    public void swapDownButton()
+    {
+        PlayerInput.sendInput(PlayerInput.Axis.SwapDown);
+    }
+
 }
